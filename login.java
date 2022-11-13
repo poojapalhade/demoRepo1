9da518948581b4cc9cea8644b0ebc0f9fe71349c
Pooja <poojanaokar@gmail.com>
2022-11-13 16:07:05 +0530

    First commit

---
+++ login.java
@@ -0,0 +1,16 @@
+public class Login
+{
+	public static login()
+	{
+		System.out.println("Login successfully");
+	}
+	public static logout()
+	{
+		System.out.println("Logout successfully");
+	}
+	public static void main(String args[])
+	{
+		login("ABC",123);
+		
+	}
+}
